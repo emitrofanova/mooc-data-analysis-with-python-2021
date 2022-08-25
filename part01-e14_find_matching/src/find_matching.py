bfec3bdfea380ffd45dadf986d7dd0abe25dc0ca
emitrofanova <e.mitrofanova.work@gmail.com>
2022-08-25 16:11:56 +0300

    From emitrofanova

--- part01-e14_find_matching/src/find_matching.py
+++ part01-e14_find_matching/src/find_matching.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 def find_matching(L, pattern):
-    return []
+    answ = []
+    for i, x in enumerate(L):
+        if pattern in x:
+            answ.append(i)
+    return answ
 
 def main():
-    pass
+    print(find_matching(["sensitive", "engine", "rubbish", "comment"], "en"))
 
 if __name__ == "__main__":
     main()
