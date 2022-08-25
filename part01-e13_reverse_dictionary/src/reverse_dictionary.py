b37ccd8120d92783c6283d59a5dff98b9de7ecfc
emitrofanova <e.mitrofanova.work@gmail.com>
2022-08-25 15:57:05 +0300

    From emitrofanova

--- part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def reverse_dictionary(d):
-    pass
+    dct = {}
+    for word in d:
+        print(d[word])
+        for i in d[word]:
+            if i not in dct:
+                dct[i] = [word]
+            else:
+                dct[i].append(word)
+    return dct
 
 def main():
     print(reverse_dictionary({'move': ['liikuttaa'], 'hide': ['piilottaa', 'salata'], 'six': ['kuusi'], 'fir': ['kuusi']}))
