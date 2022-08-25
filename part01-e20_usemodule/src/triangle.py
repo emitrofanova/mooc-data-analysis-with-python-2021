9f15372604077dc7aa0547e23cc3f11600735e10
emitrofanova <e.mitrofanova.work@gmail.com>
2022-08-25 17:51:23 +0300

    from emitrofanova

--- part01-e20_usemodule/src/triangle.py
+++ part01-e20_usemodule/src/triangle.py
@@ -1,1 +1,13 @@
 # Enter you module contents here
+import math
+def hypothenuse(side1, side2):
+    "returns the length of the hypothenuse when given the lengths of two other sides of a right-angled triangle"
+    return math.sqrt(side1**2 + side2**2)
+
+def area(side1, side2):
+    "returns the area of the right-angled triangle, when two sides, perpendicular to each other, are given as parameters"
+    return side1 * side2 / 2
+
+__doc__ = "hypothenuse and area functions for the right-angled triangle, when two sides, perpendicular to each other, are given as parameters"
+__version__ = "1.0"
+__author__ = "emitrofanova"
--- part01-e20_usemodule/src/usemodule.py
+++ part01-e20_usemodule/src/usemodule.py
@@ -7,6 +7,12 @@ except ModuleNotFoundError:
     import triangle
 
 def main():
+    print(triangle.hypothenuse(3, 4))
+    print(triangle.area(3, 4))
+    print(triangle.hypothenuse.__doc__)
+    print(triangle.__doc__)
+    print(triangle.__version__)
+    print(triangle.__author__)
     # Call the functions from here
 
 if __name__ == "__main__":
