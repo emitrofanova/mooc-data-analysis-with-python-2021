348e32538ccea14609b41d6f39d5de382af60bc1
emitrofanova <e.mitrofanova.work@gmail.com>
2022-08-25 16:46:27 +0300

    From emitrofanova

--- part01-e16_transform/src/transform.py
+++ part01-e16_transform/src/transform.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 def transform(s1, s2):
-    return []
+    return [nums[0] * nums[1] for nums in zip(map(int, s1.split()), map(int, s2.split()))] 
 
 def main():
-    pass
+    print(transform("1 5 3", "2 6 -1"))
 
 if __name__ == "__main__":
     main()
