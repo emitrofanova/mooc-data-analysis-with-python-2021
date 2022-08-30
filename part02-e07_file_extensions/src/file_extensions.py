e0a7dd046e8863c8c21537689f216bdf7fe33a28
emitrofanova <e.mitrofanova.work@gmail.com>
2022-08-30 16:12:58 +0300

    From emitrofanova

--- part02-e07_file_extensions/src/file_extensions.py
+++ part02-e07_file_extensions/src/file_extensions.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 
 def file_extensions(filename):
-    return ([], {})
+    with open(filename, "r") as f:
+        dct = {}
+        lst = []
+        for line in f:
+            line = line[:-1]
+            if "." in line:
+                ext = line.split(".")[-1]
+                if ext not in dct:
+                    dct[ext] = []
+                dct[ext].append(line)
+            else:
+                lst.append(line)
+    return (lst, dct)
 
 def main():
-    pass
+    files = file_extensions("src/filenames.txt")
+    print(len(files[0]), "files with no extension")
+    for ext in sorted(files[1]):
+        print(ext, len(files[1][ext]))
 
 if __name__ == "__main__":
     main()
