39f617a51b93ba26832d09961f6d1800764725b9
emitrofanova <e.mitrofanova.work@gmail.com>
2022-08-30 15:13:37 +0300

    From emitrofanova

--- part02-e04_word_frequencies/src/word_frequencies.py
+++ part02-e04_word_frequencies/src/word_frequencies.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 def word_frequencies(filename):
-    return {}
+    answ = {}
+    with open(filename, "r") as f:
+        text = f.read()
+    text = text.split()
+    for word in text:
+        word = word.strip("""!"#$%&'()*,-./:;?@[]_""")
+        if word not in answ:
+            answ[word] = 0
+        answ[word] += 1
+    return answ
 
 def main():
-    pass
+    print(word_frequencies("src/alice.txt"))
 
 if __name__ == "__main__":
     main()
