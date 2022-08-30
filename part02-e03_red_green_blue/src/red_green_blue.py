9f31e7cdfa264e7584d1fe978be27215da3f679b
emitrofanova <e.mitrofanova.work@gmail.com>
2022-08-30 15:02:10 +0300

    From emitrofanova

--- part02-e03_red_green_blue/src/red_green_blue.py
+++ part02-e03_red_green_blue/src/red_green_blue.py
@@ -3,11 +3,21 @@
 import re
 
 def red_green_blue(filename="src/rgb.txt"):
-    return []
+    answ = []
+    cnt = 0
+    with open(filename, "r") as f:
+        for line in f:
+            cnt += 1
+            if cnt == 1:
+                continue
+            l = re.search(r'(\d+)\s+(\d+)\s+(\d+)\s+(\w+\s*\w*)', line)
+            new_line = "\t".join(l.groups())
+            answ.append(new_line)
+    return answ
 
 
 def main():
-    pass
+    print(red_green_blue())
 
 if __name__ == "__main__":
     main()
