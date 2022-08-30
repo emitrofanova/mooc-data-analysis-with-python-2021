aab6aa5a3d8f9418818f1a01cf4698c122f1d7b1
emitrofanova <e.mitrofanova.work@gmail.com>
2022-08-30 14:31:48 +0300

    From emitrofanova

--- part02-e02_file_listing/src/file_listing.py
+++ part02-e02_file_listing/src/file_listing.py
@@ -4,10 +4,16 @@ import re
 
 
 def file_listing(filename="src/listing.txt"):
-    return []
+    with open(filename, "r") as f:
+        answ = []
+        for line in f:
+            l = re.search(r'(\d+)\s+([a-zA-Z]+)\s+(\d+)\s+(\d+)[:]+(\d+)\s+([\w\.]+)', line)
+            new_line = int(l.groups(1)[0]), l.groups(1)[1], int(l.groups(1)[2]), int(l.groups(1)[3]), int(l.groups(1)[4]), l.groups(1)[5]
+            answ.append(new_line)
+    return answ
 
 def main():
-    pass
+    print(file_listing())
 
 if __name__ == "__main__":
     main()
