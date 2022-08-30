14e5ed28e6335fa724b494d9b51502ab98ac64a3
emitrofanova <e.mitrofanova.work@gmail.com>
2022-08-30 15:59:37 +0300

    From emitrofanova

--- part02-e06_file_count/src/file_count.py
+++ part02-e06_file_count/src/file_count.py
@@ -3,10 +3,21 @@
 import sys
 
 def file_count(filename):
-    return (0, 0, 0)
+    with open(filename, "r") as f:
+        linecount = 0
+        wordcount = 0
+        charactercount = 0
+        for line in f:
+            linecount += 1
+            words = line.split()
+            wordcount += len(words)
+            charactercount += len(line)
+    return (linecount, wordcount, charactercount)
 
 def main():
-    pass
+    for f in sys.argv[1:]:
+        nums = file_count(f)
+        print(f"{nums[0]}\t{nums[1]}\t{nums[2]}\t{f}")
 
 if __name__ == "__main__":
     main()
