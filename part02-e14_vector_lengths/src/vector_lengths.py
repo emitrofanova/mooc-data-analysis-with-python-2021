a4fcbddf600ceb36686e9c7d9475d711ac8015cc
emitrofanova <e.mitrofanova.work@gmail.com>
2022-09-05 13:58:18 +0300

    From emitrofanova

--- part02-e14_vector_lengths/src/vector_lengths.py
+++ part02-e14_vector_lengths/src/vector_lengths.py
@@ -4,10 +4,15 @@ import numpy as np
 #import scipy.linalg
 
 def vector_lengths(a):
-    return np.array([])
+    a = a**2
+    answ = np.sum(a, axis = 1)
+    answ = np.sqrt(answ)
+    return answ
 
 def main():
-    pass
+    np.random.seed(9)
+    a=np.random.randint(0, 10, (3,4))
+    print(vector_lengths(a))
 
 if __name__ == "__main__":
     main()
