d95163a0d91750ec15e9f12a96ed5ad55ff81084
emitrofanova <e.mitrofanova.work@gmail.com>
2022-09-05 13:37:07 +0300

    From emitrofanova

--- part02-e13_diamond/src/diamond.py
+++ part02-e13_diamond/src/diamond.py
@@ -3,10 +3,19 @@
 import numpy as np
 
 def diamond(n):
-    return np.array([])
+    ar_r_up = np.eye(n, dtype = int)
+    ar_l_up = ar_r_up[::-1]
+    ar_r_up = ar_r_up[:,1:]
+    ar_lr_up = np.concatenate((ar_l_up, ar_r_up), axis = 1)
+    answ = ar_lr_up
+    for row in ar_lr_up[n-2::-1]:
+        if row.all() == [1]:
+            continue
+        answ = np.concatenate((answ, row.reshape(1, len(row))))
+    return answ
 
 def main():
-    pass
+    print(diamond(4))
 
 if __name__ == "__main__":
     main()
